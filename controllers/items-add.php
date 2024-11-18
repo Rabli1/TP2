101e09a1cf9868547ee68fedcdd11cdbc9d8387b
Harshinul <harshinul@gmail.com>
2024-11-18 11:49:14 -0500

    kaka

--- controllers/items-add.php
+++ controllers/items-add.php
@@ -2,8 +2,14 @@
 
 require 'src/database.php';
 require 'models/items.php';
+require 'models/category.php';
 
 $pdo = databaseGetPDO(CONFIGURATIONS['database'],DB_PARAMS);
 $item = itemsGetById($pdo,1);
 
+$name = '';
+$description = '';
+$price = '';
+$image = ''; 
+$idCategory = 1;
 require 'views/items-add.php';
